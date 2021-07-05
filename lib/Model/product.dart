0c673ed006d43675f62444feca1b61c7d6ede04d
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-07-05 10:44:15 +0430

    flutter_shop

---
+++ lib/Model/product.dart
@@ -0,0 +1,9 @@
+class Product {
+  int id;
+  String title;
+  int price;
+  String img_url;
+  String description;
+
+  Product({this.id, this.title, this.price, this.img_url, this.description});
+}
