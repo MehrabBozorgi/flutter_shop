961b11c21f8260e743d1d5b42a2a089dd43a6363
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-07-08 11:18:44 +0430

    flutter_shop

---
+++ lib/Widgets/kConst.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kToolbarAll = TextStyle(fontFamily: 'desc', fontSize: 16);
+const kToolbarNew =
+    TextStyle(fontFamily: 'desc', fontSize: 16, color: Colors.black);
